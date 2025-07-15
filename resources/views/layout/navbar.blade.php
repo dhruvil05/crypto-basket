433b75f63d1792cf5097429e4a5292290f76cda6
Dhruvil Dvij <dhruvil.s@dvijinfotech.com>
2025-07-15 10:49:33 +0530

    sign out button issue

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,11 +3,15 @@
         <a href="#" class="logo">{{ env('APP_NAME') }}</a>
         <ul class="nav-links">
             @if (auth()->check())
-                <li><a href="{{route('platform.logout')}}">Sign out</a></li>
-                <li><a href="{{route('platform.baskets')}}">Dashboard</a></li>
+                <li><a href="{{ route('platform.baskets') }}">Dashboard</a></li>
+
+                <form method="POST" action="{{ route('platform.logout') }}">
+                    @csrf
+                    <button type="submit">Sign out</button>
+                </form>
             @else
-                <li><a href="{{route('platform.login')}}">Sign in</a></li>
-                <li><a href="{{route('platform.register')}}">Sign up</a></li>
+                <li><a href="{{ route('platform.login') }}">Sign in</a></li>
+                <li><a href="{{ route('platform.register') }}">Sign up</a></li>
             @endif
         </ul>
         {{-- <a href="#" class="cta-button">Sign in</a>
